3f74f95e2904c7393c122573212401e5ad2acd1f
Ravendra Patel <pawan.developers@gmail.com>
2024-12-18 18:30:25 +0530

    type issue fixed

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,18 +1,16 @@
 
 import { PrismaClient, Book } from "@prisma/client";
-
+import BookFilters  from './filters'
 
 const prisma = new PrismaClient();
+type SearchParams = Record<string, string | string[] | undefined>;
 
-export default async function Books({
-    searchParams
-  }: {
-    searchParams: Record<string, string | string[] | undefined>
-  }) {
+
+export default async function Books({ searchParams }: { searchParams: SearchParams }) {
     
-    const genre = typeof searchParams.genre === 'string' && searchParams.genre !== ''  ? searchParams.genre : undefined;
-    const sort = searchParams.sort === "asc" || searchParams.sort === "desc" ? searchParams.sort : 'asc';
-
+    const genre = typeof searchParams['genre'] === 'string' && searchParams['genre'] !== ''  ? searchParams['genre'] : undefined;
+    const sort = typeof searchParams['sort'] === 'string' && searchParams['sort'] == 'desc'  ? 'desc' : 'asc';
+  
     const whereClause = genre ? { genre } : {};
 
     const books = await prisma.book.findMany({
@@ -30,10 +28,10 @@ export default async function Books({
 
 
     return (
-        <div className="grid grid-rows-[20px_1fr_20px]  justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <div className="grid grid-rows-[20px_1fr_20px]  justify-items-center min-h-screen  pb-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <h1 className="mb-4 text-4xl">Books({books.length})</h1>
-                
+                <BookFilters genre={genre} sort={sort} genres={genreOptions} />
                 <ul>
                     {books.map((book:Book, index) => (
                         <li key={book.id} className="mb-2">
